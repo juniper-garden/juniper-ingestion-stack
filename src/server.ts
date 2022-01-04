5a37373013d7b978ba7a3400b4751723ac7a55c3
Daniel <daniel.ashcraft@ofashandfire.com>
2022-01-03 21:46:18 -0600

    even more configurations

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,14 @@ if (process.env.USE_KAFKA) {
   setupKafka()
   async function setupKafka() {
     const producer = kafka.producer()
-    const consumer = kafka.consumer({ groupId: 'nodejs-dev' })
+    const consumer = kafka.consumer({
+      groupId: 'nodejs-dev',
+      retry: {
+        initialRetryTime: 10 * 1000,
+        retries: 10
+      },
+      heartbeatInterval: 10 * 1000
+    })
     await producer.connect()
 
     app.set('kafka_producer', producer)
