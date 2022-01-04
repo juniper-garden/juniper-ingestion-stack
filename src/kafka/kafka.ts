f70d5abd3b0627ee7aa17a13e47a166d53b20089
Daniel <daniel.ashcraft@ofashandfire.com>
2022-01-03 18:11:51 -0600

    update to hard code broker

--- src/kafka/kafka.ts
+++ src/kafka/kafka.ts
@@ -2,10 +2,11 @@ const { Kafka } = require('kafkajs')
 
 let kafka:any = {}
 const brokers = process.env.KAFKA_BROKERS.split(',')
+console.log('brokers', brokers)
 // Create the client with the broker list
 if (process.env.USE_KAFKA) {
   kafka = new Kafka({
-    brokers,
+    brokers: ['157.245.114.98:9092'],
     clientId: 'juniper-ingest-client'
   })
 }
