bc5379f2be8b4926f323453de9df22a98d2b33bf
Daniel <daniel.ashcraft@ofashandfire.com>
2022-01-03 20:46:22 -0600

    Update to use broker list again

--- src/kafka/kafka.ts
+++ src/kafka/kafka.ts
@@ -6,7 +6,7 @@ console.log('brokers', brokers)
 // Create the client with the broker list
 if (process.env.USE_KAFKA) {
   kafka = new Kafka({
-    brokers: ['157.245.114.98:9092'],
+    brokers,
     clientId: 'juniper-ingest-client'
   })
 }
